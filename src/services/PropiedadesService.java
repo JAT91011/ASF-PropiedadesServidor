0d1bc3dfa2a8fe21eb081e94e809bcea949315e5
Endika <salgueiro93@gmail.com>
2016-03-10 13:25:08 +0100

    Se reformula el servicio web y se añaden más métodos.
    Además, se añade un main de prueba en la propia clase del servicio web
    (PropiedadService) de gran utilidad.

--- src/services/PropiedadesService.java
+++ src/services/PropiedadesService.java
@@ -15,41 +15,33 @@ public class PropiedadesService {
 		
 	}
 	
-	public ArrayList<Propiedad> obtenerPropiedades() throws SQLException, ClassNotFoundException {
-		ArrayList<Propiedad> alp = new ArrayList<Propiedad>();
-		
+	public Propiedad[] obtenerPropiedades() throws SQLException, ClassNotFoundException {
 		BaseDatos.obtenerInstancia().conectar();
-		alp = BaseDatos.obtenerInstancia().obtenetPropiedades();
+		Propiedad [] p = BaseDatos.obtenerInstancia().obtenetPropiedades();
 		BaseDatos.obtenerInstancia().desconectar();
 		
-		return alp;
+		return p;
 	}
 	
-	public ArrayList<Propiedad> obtenerPropiedadesPorMunicipio (String municipio) throws SQLException, ClassNotFoundException {
-		ArrayList<Propiedad> alp = new ArrayList<Propiedad>();
-		
+	public Propiedad[] obtenerPropiedadesPorMunicipio (String municipio) throws SQLException, ClassNotFoundException {
 		BaseDatos.obtenerInstancia().conectar();
-		alp = BaseDatos.obtenerInstancia().obtenetPropiedadesPorMunicipio(municipio);
+		Propiedad [] p = BaseDatos.obtenerInstancia().obtenetPropiedadesPorMunicipio(municipio);
 		BaseDatos.obtenerInstancia().desconectar();
 		
-		return alp;
+		return p;
 	}
 	
-	public ArrayList<Propiedad> obtenerPropiedadesPorNombre (String nombre) throws SQLException, ClassNotFoundException {
-		ArrayList<Propiedad> alp = new ArrayList<Propiedad>();
-		
+	public Propiedad[] obtenerPropiedadesPorNombre (String nombre) throws SQLException, ClassNotFoundException {
 		BaseDatos.obtenerInstancia().conectar();
-		alp = BaseDatos.obtenerInstancia().obtenetPropiedadesPorNombre(nombre);
+		Propiedad [] p = BaseDatos.obtenerInstancia().obtenetPropiedadesPorNombre(nombre);
 		BaseDatos.obtenerInstancia().desconectar();
 		
-		return alp;
+		return p;
 	}
 	
 	public Propiedad obtenerPropiedad (int id) throws SQLException, ClassNotFoundException {
-		Propiedad p = new Propiedad();
-		
 		BaseDatos.obtenerInstancia().conectar();
-		p = BaseDatos.obtenerInstancia().obtenerPropiedad(id);
+		Propiedad p = BaseDatos.obtenerInstancia().obtenerPropiedad(id);
 		BaseDatos.obtenerInstancia().desconectar();
 		
 		return p;
@@ -71,5 +63,147 @@ public class PropiedadesService {
 		BaseDatos.obtenerInstancia().conectar();
 		BaseDatos.obtenerInstancia().borrarPropiedad(id);
 		BaseDatos.obtenerInstancia().desconectar();
+	}
+	
+	public boolean existeProvinciaConNombre (String provincia) throws SQLException, ClassNotFoundException {
+		BaseDatos.obtenerInstancia().conectar();
+		boolean resul = BaseDatos.obtenerInstancia().existeProvinciaConNombre(provincia);
+		BaseDatos.obtenerInstancia().desconectar();
+		
+		return resul;
+	}
+	
+	public void insertarProvincia (Provincia p) throws ClassNotFoundException, SQLException {
+		BaseDatos.obtenerInstancia().conectar();
+		BaseDatos.obtenerInstancia().insertarProvincia(p);
+		BaseDatos.obtenerInstancia().desconectar();
+	}
+	
+	public Provincia obtenerProvinciaPorNombre (String provincia) throws ClassNotFoundException, SQLException {
+		BaseDatos.obtenerInstancia().conectar();
+		Provincia p = BaseDatos.obtenerInstancia().obtenerProvinciaPorNombre(provincia);
+		BaseDatos.obtenerInstancia().desconectar();
+		
+		return p;
+	}
+	
+	public boolean existeMunicipioConNombre (String nomMunicipio) throws ClassNotFoundException, SQLException {
+		BaseDatos.obtenerInstancia().conectar();
+		boolean resul = BaseDatos.obtenerInstancia().existeMunicipioConNombre(nomMunicipio);
+		BaseDatos.obtenerInstancia().desconectar();
+		
+		return resul;
+	}
+	
+	public void insertarMunicipio (Municipio m) throws ClassNotFoundException, SQLException {
+		BaseDatos.obtenerInstancia().conectar();
+		BaseDatos.obtenerInstancia().insertarMunicipio(m);
+		BaseDatos.obtenerInstancia().desconectar();
+	}
+	
+	public Municipio obtenerMunicipioPorNombre (String nomMunicipio) throws ClassNotFoundException, SQLException {
+		BaseDatos.obtenerInstancia().conectar();
+		Municipio m = BaseDatos.obtenerInstancia().obtenerMunicipioPorNombre(nomMunicipio);
+		BaseDatos.obtenerInstancia().desconectar();
+		
+		return m;
+	}
+	
+	public static void main (String [] args) throws SQLException, ClassNotFoundException {
+		// Declaramos el servicio
+		PropiedadesService ps = new PropiedadesService();
+		
+		// Provincia Madrid
+		if (!ps.existeProvinciaConNombre("Madrid")) {
+			ps.insertarProvincia(new Provincia("Madrid"));
+		}
+		Provincia p_Madrid = ps.obtenerProvinciaPorNombre("Madrid");
+		
+		// Provincia Vizcaya
+		if (!ps.existeProvinciaConNombre("Vizcaya")) {
+			ps.insertarProvincia(new Provincia("Vizcaya"));
+		}
+		Provincia p_Vizcaya = ps.obtenerProvinciaPorNombre("Vizcaya");
+		
+		// Provincia Alava
+		if (!ps.existeProvinciaConNombre("Alava")) {
+			ps.insertarProvincia(new Provincia("Alava"));
+		}
+		Provincia p_Alava = ps.obtenerProvinciaPorNombre("Alava");
+		
+		// Municipio Bilbao
+		if (!ps.existeMunicipioConNombre("Bilbao")) {
+			ps.insertarMunicipio(new Municipio("Bilbao", p_Vizcaya));
+		}
+		Municipio m_Bilbao = ps.obtenerMunicipioPorNombre("Bilbao");
+		
+		// Municipio Karrantza
+		if (!ps.existeMunicipioConNombre("Karrantza")) {
+			ps.insertarMunicipio(new Municipio("Karrantza", p_Vizcaya));
+		}
+		Municipio m_Karrantza = ps.obtenerMunicipioPorNombre("Karrantza");
+		
+		// Municipio Madrid
+		if (!ps.existeMunicipioConNombre("Madrid")) {
+			ps.insertarMunicipio(new Municipio("Madrid", p_Madrid));
+		}
+		Municipio m_Madrid = ps.obtenerMunicipioPorNombre("Madrid");
+		
+		// Municipio Vitoria
+		if (!ps.existeMunicipioConNombre("Vitoria")) {
+			ps.insertarMunicipio(new Municipio("Vitoria", p_Alava));
+		}
+		Municipio m_Vitoria = ps.obtenerMunicipioPorNombre("Vitoria");
+		
+		// Propiedades
+		Propiedad pro_Bilbao = new Propiedad("Terreno Bilbao", "grande", "Calle falsa 1234", 25.564561f, -4.5646513216f, 2500.1, 500, m_Bilbao);
+		Propiedad pro_Karrantza = new Propiedad("Terreno Karrantza", "grande", "Calle falsa 1234", 25.564561f, -4.5646513216f, 2500.1, 500, m_Karrantza);
+		Propiedad pro_Madrid = new Propiedad("Terreno Madrid", "grande", "Calle falsa 1234", 25.564561f, -4.5646513216f, 2500.1, 500, m_Madrid);
+		Propiedad pro_Vitoria = new Propiedad("Terreno Vitoria", "grande", "Calle falsa 1234", 25.564561f, -4.5646513216f, 2500.1, 500, m_Vitoria);
+		
+		// Insertamos las propiedades
+		ps.insertarPropiedad(pro_Bilbao);
+		ps.insertarPropiedad(pro_Karrantza);
+		ps.insertarPropiedad(pro_Madrid);
+		ps.insertarPropiedad(pro_Vitoria);
+		System.out.println("Insercion realizada correctamente\n");
+		
+		// Obtenemos las propiedades y las mostramos por pantalla
+		System.out.println("Propiedades insertadas:");
+		Propiedad [] propiedades = ps.obtenerPropiedades();
+		for (int i = 0; i < propiedades.length; i++) {
+			System.out.println((i+1) + ". " + propiedades[i].getNombre() + "\n");
+		}
+		
+		// Mostramos las propiedades de Bilbao
+		System.out.println("Propiedades en Bilbao:");
+		Propiedad [] propiedadesMunicipio = ps.obtenerPropiedadesPorMunicipio("Bilbao");
+		for (int i = 0; i < propiedadesMunicipio.length; i++) {
+			System.out.println((i+1) + ". " + propiedadesMunicipio[i].getNombre() + "\n");
+		}
+		
+		// Mostramos las propiedades que tengan como nombre Karrantza
+		System.out.println("Propiedades con nombre Karrantza:");
+		Propiedad [] propiedadesNombre = ps.obtenerPropiedadesPorNombre("Karrantza");
+		for (int i = 0; i < propiedadesNombre.length; i++) {
+			System.out.println((i+1) + ". " + propiedadesNombre[i].getNombre() + "\n");
+		}
+		
+		// Editamos la propiedad de Madrid
+		for (int i = 0; i < propiedades.length; i++) {
+			if (propiedades[i].getNombre().equals("Terreno Madrid")) {
+				propiedades[i].setNombre("Terreno Madrid Editado");
+				ps.editarPropiedad(propiedades[i]);
+				System.out.println("La propiedad " + propiedades[i].getNombre() + " ha sido editada correctamente.\n");
+			}
+		}
+		
+		// Borramos la propiedad de Vitoria
+		for (int i = 0; i < propiedades.length; i++) {
+			if (propiedades[i].getNombre().equals("Terreno Vitoria")) {
+				ps.borrarPropiedad(propiedades[i].getId());
+				System.out.println("La propiedad " + propiedades[i].getNombre() + " ha sido borrada correctamente.\n");
+			}
+		}
 	}
 }
--- src/utils/BaseDatos.java
+++ src/utils/BaseDatos.java
@@ -34,25 +34,30 @@ public class BaseDatos {
         con.close();
     }    
     
-    public ArrayList<Propiedad> obtenetPropiedades () throws SQLException{        
-    	ArrayList<Propiedad> ap = new ArrayList<Propiedad>();
+    // MÉTODOS SERVICIOS WEB
+    
+    public Propiedad[] obtenetPropiedades () throws SQLException{        
+    	int numProp = numPropiedades();
+    	Propiedad [] propiedades = new Propiedad [numProp];
     	
     	Statement stmt = con.createStatement();
     	String query = "SELECT * FROM propiedad";
         ResultSet rs = stmt.executeQuery(query);
         
         Municipio m = null;
+        int pos = 0;
         while (rs.next()){
         	m = obtenerMunicipio(rs.getInt("idMunicipio"));
-        	ap.add(new Propiedad(rs.getInt("idPropiedad"), rs.getString("nombre"), 
+        	propiedades[pos] = new Propiedad(rs.getInt("idPropiedad"), rs.getString("nombre"), 
         			rs.getString("descripcion"), rs.getString("direccion"), 
         			rs.getFloat("latitud"), rs.getFloat("longitud"), rs.getDouble("area"), 
-        			rs.getDouble("precio"), m));
+        			rs.getDouble("precio"), m);
+        	pos++;
         }
         rs.close();
         stmt.close(); 
         
-        return ap;
+        return propiedades;
     }
     
     public Propiedad obtenerPropiedad (int id) throws SQLException {
@@ -73,97 +78,73 @@ public class BaseDatos {
     	return p;
     }
     
-    public ArrayList<Propiedad> obtenetPropiedadesPorMunicipio (String municipio) throws SQLException{        
-    	ArrayList<Propiedad> ap = new ArrayList<Propiedad>();
+    public Propiedad[] obtenetPropiedadesPorMunicipio (String municipio) throws SQLException{        
+    	int numProp = numPropiedadesEnMunicipio(municipio);
+    	Propiedad [] propiedades = new Propiedad [numProp];
     	Municipio m = obtenerMunicipioPorNombre(municipio);
     	
-    	if (m != null) {
+    	if (m != null && numProp > 0) {
     		Statement stmt = con.createStatement();
         	String query = "SELECT * FROM propiedad WHERE idMunicipio = " + m.getId();
             ResultSet rs = stmt.executeQuery(query);
             
+            int pos = 0;
             while (rs.next()){
-            	ap.add(new Propiedad(rs.getInt("idPropiedad"), rs.getString("nombre"), 
+            	propiedades [pos] = new Propiedad(rs.getInt("idPropiedad"), rs.getString("nombre"), 
             			rs.getString("descripcion"), rs.getString("direccion"), 
             			rs.getFloat("latitud"), rs.getFloat("longitud"), rs.getDouble("area"), 
-            			rs.getDouble("precio"), m));
+            			rs.getDouble("precio"), m);
+            	pos++;
             }
             rs.close();
             stmt.close();
-    	} 
-        
-        return ap;
+            
+            return propiedades;
+    	} else {
+    		return null;
+    	}
     }
     
-    public ArrayList<Propiedad> obtenetPropiedadesPorNombre (String nombre) throws SQLException{        
-    	ArrayList<Propiedad> ap = new ArrayList<Propiedad>();
+    public Propiedad[] obtenetPropiedadesPorNombre (String nombre) throws SQLException{        
+    	int numProp = numPropiedadesConNombre(nombre);
+    	Propiedad [] propiedades = new Propiedad [numProp];
     	
     	Statement stmt = con.createStatement();
     	String query = "SELECT * FROM propiedad WHERE nombre LIKE '%" + nombre + "%'";
         ResultSet rs = stmt.executeQuery(query);
         
         Municipio m = null;
+        int pos = 0;
         while (rs.next()){
         	m = obtenerMunicipio(rs.getInt("idMunicipio"));
-        	ap.add(new Propiedad(rs.getInt("idPropiedad"), rs.getString("nombre"), 
+        	propiedades [pos] = new Propiedad(rs.getInt("idPropiedad"), rs.getString("nombre"), 
         			rs.getString("descripcion"), rs.getString("direccion"), 
         			rs.getFloat("latitud"), rs.getFloat("longitud"), rs.getDouble("area"), 
-        			rs.getDouble("precio"), m));
+        			rs.getDouble("precio"), m);
+        	pos++;
         }
         rs.close();
         stmt.close();
         
-        return ap;
+        return propiedades;
     }
     
     public void insertarPropiedad (Propiedad p) throws SQLException {
-    	Municipio municipio = p.getMunicipio();
-    	Provincia provincia = municipio.getProvincia();
-    	
-    	if (obtenerProvinciaPorNombre(provincia.getNombre()) == null) {
-    		insertarProvincia(provincia);
-    	}
-    	
-    	provincia = obtenerProvinciaPorNombre(provincia.getNombre());
-    	
-    	if (obtenerMunicipioPorNombre(municipio.getNombre()) == null) {
-    		municipio.setProvincia(provincia);
-    		insertarMunicipio(municipio);
-    	}
-    	
-    	municipio = obtenerMunicipioPorNombre(municipio.getNombre());
-    	
     	Statement stmt = con.createStatement();
     	String query = "INSERT INTO propiedad (nombre, descripcion, direccion, latitud, longitud, "
     			+ "area, precio, idMunicipio) VALUES ('" + p.getNombre() + "', '" + p.getDescripcion() + "', "
     					+ "'" + p.getDireccion() + "', " + p.getLatitud() + ", " + p.getLongitud() + ", "
-    							+ p.getArea() +", " + p.getPrecio() + ", " + municipio.getId() + ")";
+    							+ p.getArea() +", " + p.getPrecio() + ", " + p.getMunicipio().getId() + ")";
     	stmt.executeUpdate(query);
     	stmt.close();
     }
     
     public void editarPropiedad (Propiedad p) throws SQLException {
-    	Municipio municipio = p.getMunicipio();
-    	Provincia provincia = municipio.getProvincia();
-    	
-    	if (obtenerProvinciaPorNombre(provincia.getNombre()) == null) {
-    		insertarProvincia(provincia);
-    	}
-    	
-    	provincia = obtenerProvinciaPorNombre(provincia.getNombre());
-    	
-    	if (obtenerMunicipioPorNombre(municipio.getNombre()) == null) {
-    		municipio.setProvincia(provincia);
-    		insertarMunicipio(municipio);
-    	}
-    	
-    	municipio = obtenerMunicipioPorNombre(municipio.getNombre());
-    	
     	Statement stmt = con.createStatement();
     	String query = "UPDATE propiedad SET nombre = '" + p.getNombre() + "', descripcion = '" + p.getDescripcion() + "',"
     			+ " direccion = '" + p.getDireccion() + "', latitud = " + p.getLatitud() + ", "
     					+ "longitud = " + p.getLongitud() + ", area = " + p.getArea() + ", "
-    							+ "precio = " + p.getPrecio() + ", idMunicipio = " + municipio.getId() + " WHERE "
+    							+ "precio = " + p.getPrecio() + ", idMunicipio = " + p.getMunicipio().getId() + " WHERE "
     									+ "idPropiedad = " + p.getId();
     	stmt.executeUpdate(query);
     	stmt.close();
@@ -176,23 +157,67 @@ public class BaseDatos {
     	stmt.close();
     }
     
-    public Municipio obtenerMunicipio (int id) throws SQLException {
-    	Municipio m = null;
-    	
+    public boolean existeProvinciaConNombre (String provincia) throws SQLException {
     	Statement stmt = con.createStatement();
-    	String query = "SELECT * FROM municipio WHERE idMunicipio = " + id;
+		String query = "SELECT * FROM provincia WHERE nombre = '" + provincia + "'";
         ResultSet rs = stmt.executeQuery(query);
         
-        Provincia p = null;
         if (rs.next()) {
-        	p = obtenerProvincia(rs.getInt("idProvincia"));
-        	m = new Municipio(rs.getInt("idMunicipio"), rs.getString("nombre"), 
-        			p);
+        	rs.close();
+            stmt.close();
+        	return true;
+        } else {
+        	rs.close();
+            stmt.close();
+        	return false;
+        }
+    }
+    
+    public void insertarProvincia (Provincia p) throws SQLException {
+    	Statement stmt = con.createStatement();
+    	String query = "INSERT INTO provincia (nombre) VALUES ('" + p.getNombre() + "')";
+    	stmt.executeUpdate(query);
+    	stmt.close();
+    }
+    
+    public Provincia obtenerProvinciaPorNombre (String provincia) throws SQLException {
+    	Provincia p = null;
+    	
+    	Statement stmt = con.createStatement();
+		String query = "SELECT * FROM provincia WHERE nombre = '" + provincia + "'";
+        ResultSet rs = stmt.executeQuery(query);
+        
+        if (rs.next()) {
+        	p = new Provincia(rs.getInt("idProvincia"), rs.getString("nombre"));
         }
         rs.close();
         stmt.close();
         
-        return m;
+        return p;
+    }
+    
+    public boolean existeMunicipioConNombre (String nomMunicipio) throws SQLException {
+    	Statement stmt = con.createStatement();
+    	String query = "SELECT * FROM municipio WHERE nombre = '" + nomMunicipio + "'";
+        ResultSet rs = stmt.executeQuery(query);
+        
+        if (rs.next()) {
+        	rs.close();
+        	stmt.close();
+        	return true;
+        } else {
+        	rs.close();
+            stmt.close();
+            return false;
+        }
+    }
+    
+    public void insertarMunicipio (Municipio m) throws SQLException {
+    	Statement stmt = con.createStatement();
+    	String query = "INSERT INTO municipio (nombre, idProvincia) VALUES ('" + m.getNombre() + "',"
+    			+ " " + m.getProvincia().getId() + ")";
+    	stmt.executeUpdate(query);
+    	stmt.close();
     }
     
     public Municipio obtenerMunicipioPorNombre (String nomMunicipio) throws SQLException {
@@ -214,12 +239,25 @@ public class BaseDatos {
         return m;
     }
     
-    public void insertarMunicipio (Municipio m) throws SQLException {
+    // MÉTODOS AUXILIARES
+    
+    public Municipio obtenerMunicipio (int id) throws SQLException {
+    	Municipio m = null;
+    	
     	Statement stmt = con.createStatement();
-    	String query = "INSERT INTO municipio (nombre, idProvincia) VALUES ('" + m.getNombre() + "',"
-    			+ " " + m.getProvincia().getId() + ")";
-    	stmt.executeUpdate(query);
-    	stmt.close();
+    	String query = "SELECT * FROM municipio WHERE idMunicipio = " + id;
+        ResultSet rs = stmt.executeQuery(query);
+        
+        Provincia p = null;
+        if (rs.next()) {
+        	p = obtenerProvincia(rs.getInt("idProvincia"));
+        	m = new Municipio(rs.getInt("idMunicipio"), rs.getString("nombre"), 
+        			p);
+        }
+        rs.close();
+        stmt.close();
+        
+        return m;
     }
     
     public Provincia obtenerProvincia (int id) throws SQLException {
@@ -238,34 +276,55 @@ public class BaseDatos {
         return p;
     }
     
-    public Provincia obtenerProvinciaPorNombre (String provincia) {
-    	Provincia p = null;
+    public int numPropiedades() throws SQLException {
+    	Statement stmt = con.createStatement();
+    	String query = "SELECT COUNT(*) FROM propiedad";
+    	ResultSet rs = stmt.executeQuery(query);
     	
-    	Statement stmt;
-		try {
-			stmt = con.createStatement();
-			String query = "SELECT * FROM provincia WHERE nombre = '" + provincia + "'";
-	        ResultSet rs = stmt.executeQuery(query);
-	        
-	        if (rs.next()) {
-	        	p = new Provincia(rs.getInt("idProvincia"), rs.getString("nombre"));
-	        }
-	        rs.close();
-	        stmt.close();
-	        
-	        return p;
-		} catch (SQLException e) {
-			System.out.println("Entra aqui");
-			e.printStackTrace();
-			return null;
-		}
+    	int num = 0;
+    	if (rs.next()) {
+    		num = rs.getInt(1);
+    	}
+    	
+    	rs.close();
+    	stmt.close();
+    	return num;
     }
     
-    public void insertarProvincia (Provincia p) throws SQLException {
+    public int numPropiedadesEnMunicipio(String municipio) throws SQLException {
+    	Municipio m = obtenerMunicipioPorNombre(municipio);
+    	
+    	if (m != null) {
+    		Statement stmt = con.createStatement();
+        	String query = "SELECT COUNT(*) FROM propiedad WHERE idMunicipio = " + m.getId();
+        	ResultSet rs = stmt.executeQuery(query);
+        	
+        	int num = 0;
+        	if (rs.next()) {
+        		num = rs.getInt(1);
+        	}
+        	
+        	rs.close();
+        	stmt.close();
+        	return num;
+    	} else {
+    		return 0;
+    	}
+    }
+    
+    public int numPropiedadesConNombre(String nombre) throws SQLException {
     	Statement stmt = con.createStatement();
-    	String query = "INSERT INTO provincia (nombre) VALUES ('" + p.getNombre() + "')";
-    	stmt.executeUpdate(query);
+    	String query = "SELECT COUNT(*) FROM propiedad WHERE nombre LIKE '%" + nombre + "%'";
+    	ResultSet rs = stmt.executeQuery(query);
+    	
+    	int num = 0;
+    	if (rs.next()) {
+    		num = rs.getInt(1);
+    	}
+    	
+    	rs.close();
     	stmt.close();
+    	return num;
     }
     
     public static BaseDatos obtenerInstancia() {
